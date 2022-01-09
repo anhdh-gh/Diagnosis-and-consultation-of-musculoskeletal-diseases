fbb29c513bb9d16ade166bed405a583ff9136bdd
Đỗ Hùng Anh <dohunganh27092000@gmail.com>
2022-01-09 19:57:05 +0700

    Initial commit

---
+++ src/main/java/he_thong_tu_van_benh_xuong_khop__thi_chtdttt/HeThongTuVanBenhXuongKhopThiChtdtttApplication.java
@@ -0,0 +1,13 @@
+package he_thong_tu_van_benh_xuong_khop__thi_chtdttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeThongTuVanBenhXuongKhopThiChtdtttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeThongTuVanBenhXuongKhopThiChtdtttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/he_thong_tu_van_benh_xuong_khop__thi_chtdttt/HeThongTuVanBenhXuongKhopThiChtdtttApplicationTests.java
@@ -0,0 +1,13 @@
+package he_thong_tu_van_benh_xuong_khop__thi_chtdttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeThongTuVanBenhXuongKhopThiChtdtttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
