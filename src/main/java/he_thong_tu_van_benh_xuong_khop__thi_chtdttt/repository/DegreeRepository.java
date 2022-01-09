c1b1056e7ccacf6a0bdb39f1936494ad081b524f
Đỗ Hùng Anh <dohunganh27092000@gmail.com>
2022-01-10 01:24:23 +0700

    Push repository

--- src/main/java/he_thong_tu_van_benh_xuong_khop__thi_chtdttt/repository/DegreeRepository.java
+++ src/main/java/he_thong_tu_van_benh_xuong_khop__thi_chtdttt/repository/DegreeRepository.java
@@ -2,6 +2,8 @@ package he_thong_tu_van_benh_xuong_khop__thi_chtdttt.repository;
 
 import he_thong_tu_van_benh_xuong_khop__thi_chtdttt.entity.Degree;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DegreeRepository extends JpaRepository<Degree, Integer> {
 }
--- src/main/java/he_thong_tu_van_benh_xuong_khop__thi_chtdttt/repository/DiseaseRepository.java
+++ src/main/java/he_thong_tu_van_benh_xuong_khop__thi_chtdttt/repository/DiseaseRepository.java
@@ -2,6 +2,8 @@ package he_thong_tu_van_benh_xuong_khop__thi_chtdttt.repository;
 
 import he_thong_tu_van_benh_xuong_khop__thi_chtdttt.entity.Disease;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DiseaseRepository extends JpaRepository<Disease, Integer> {
 }
